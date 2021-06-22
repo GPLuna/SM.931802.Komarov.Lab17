4bba9f94c76187949d2ffa90bbfb65ffff358c21
GPLuna <GPLuna@mail.ru>
2021-06-22 18:15:05 +0700

    Добавьте файлы проекта.

---
+++ Lab17/Lab17/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Lab17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lam1 = new System.Windows.Forms.NumericUpDown();
+            this.lam2 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.lam1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lam2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "λ1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(16, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "λ2";
+            // 
+            // lam1
+            // 
+            this.lam1.DecimalPlaces = 2;
+            this.lam1.Location = new System.Drawing.Point(65, 23);
+            this.lam1.Name = "lam1";
+            this.lam1.Size = new System.Drawing.Size(120, 23);
+            this.lam1.TabIndex = 2;
+            this.lam1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lam2
+            // 
+            this.lam2.DecimalPlaces = 2;
+            this.lam2.Location = new System.Drawing.Point(65, 69);
+            this.lam2.Name = "lam2";
+            this.lam2.Size = new System.Drawing.Size(120, 23);
+            this.lam2.TabIndex = 3;
+            this.lam2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 115);
+            this.chart1.Name = "chart1";
+            series7.BorderWidth = 3;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series7.Color = System.Drawing.Color.Magenta;
+            series7.Legend = "Legend1";
+            series7.Name = "lamda1";
+            series8.BorderWidth = 3;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series8.Color = System.Drawing.Color.Blue;
+            series8.Legend = "Legend1";
+            series8.Name = "lamda2";
+            series9.BorderWidth = 3;
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series9.Color = System.Drawing.Color.Cyan;
+            series9.Legend = "Legend1";
+            series9.Name = "l1+l2";
+            series10.BorderWidth = 3;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series10.Color = System.Drawing.Color.Blue;
+            series10.LabelBorderWidth = 3;
+            series10.Legend = "Legend1";
+            series10.MarkerBorderWidth = 3;
+            series10.MarkerStep = 3;
+            series10.Name = "Series1";
+            series10.YValuesPerPoint = 3;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series11.Color = System.Drawing.Color.Red;
+            series11.Legend = "Legend1";
+            series11.Name = "Series2";
+            series11.YValuesPerPoint = 3;
+            series12.BorderWidth = 3;
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series12.Color = System.Drawing.Color.Fuchsia;
+            series12.Legend = "Legend1";
+            series12.Name = "Series3";
+            series12.YValuesPerPoint = 3;
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Size = new System.Drawing.Size(1047, 397);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Plum;
+            this.Start.ForeColor = System.Drawing.Color.Black;
+            this.Start.Location = new System.Drawing.Point(244, 23);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(160, 69);
+            this.Start.TabIndex = 6;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(1073, 559);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lam2);
+            this.Controls.Add(this.lam1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Indigo;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lam1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lam2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown lam1;
+        private System.Windows.Forms.NumericUpDown lam2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Lab17/Lab17/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int n = 10000;
+        private void btStart_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 6; i++)
+            {
+                chart1.Series[i].Points.Clear();
+            }
+            Model P1 = new Model((double)lam1.Value, n);
+            Model P2 = new Model((double)lam2.Value, n);
+            while (P1.t > P1.Time)
+            {
+                P1.ExpRV();
+                chart1.Series[0].Points.AddXY(P1.Time, P1.lambda);
+                chart1.Series[3].Points.AddXY(P1.Time, P1.lambda);
+                chart1.Series[2].Points.AddXY(P1.Time, P1.lambda + P2.lambda);
+                chart1.Series[5].Points.AddXY(P1.Time, P1.lambda + P2.lambda);
+            }
+            while (P2.t > P2.Time)
+            {
+                P2.ExpRV();
+                chart1.Series[1].Points.AddXY(P2.Time, P2.lambda);
+                chart1.Series[4].Points.AddXY(P2.Time, P2.lambda);
+                chart1.Series[2].Points.AddXY(P2.Time, P1.lambda + P2.lambda);
+                chart1.Series[5].Points.AddXY(P2.Time, P1.lambda + P2.lambda);
+            }
+
+            int max1 = P1.SC();
+            int max2 = P2.SC();
+            int size;
+            if (max1 < max2)
+            {
+                size = max2 + 1;
+            }
+            else
+            {
+                size = max1 + 1;
+            }
+            double[] Freq = new double[size];
+            for (int i = 0; i < P1.N; i++)
+            {
+                Freq[P1.ArrayCountPoints[i]]++;
+            }
+            for (int i = 0; i < P2.N; i++)
+            {
+                Freq[P2.ArrayCountPoints[i]]++;
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Freq[i] = Freq[i] / P1.N * 2;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Model P1 = new Model((double)lam1.Value, n);
+            Model P2 = new Model((double)lam2.Value, n);
+            while (P1.t > P1.Time)
+            {
+                P1.ExpRV();
+                chart1.Series[0].Points.AddXY(P1.Time, P2.lambda);
+                chart1.Series[3].Points.AddXY(P1.Time, P2.lambda);
+            }
+        }
+    }
+}
